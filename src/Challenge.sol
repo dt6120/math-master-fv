a227a2d7fd0191abd8e6e4cfccfa01889f692cd9
Dhruv Takwal <dhruvtakwal6120@gmail.com>
2024-10-19 21:17:40 +0000

    chore: copy challenge contract addresses on sepolia

---
+++ src/Challenge.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+interface IChallenge {
+    function exp2(uint256 x) external returns (uint256);
+}
+
+address constant CHALLENGE_1 = 0x3DbBF2F9AcFB9Aac8E0b31563dd75a2D69148D64;
+address constant CHALLENGE_2 = 0xf2dde8E99f583b5354441EB7a141042419082596;
